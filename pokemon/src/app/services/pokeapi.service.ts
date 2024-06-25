1ebcee1ca74c1959ff4666af09c937f0bfbe5eda
Adrian Hoyos <adrianh_589@hotmail.com>
2024-06-25 15:36:16 -0500

    Ahora se ven en orden los pokemons

--- pokemon/src/app/services/pokeapi.service.ts
+++ pokemon/src/app/services/pokeapi.service.ts
@@ -1,50 +1,50 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {combineAll, concatAll, map, mapTo, mergeMap, toArray} from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { concatMap, map, toArray } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeapiService {
 
-  constructor( private http: HttpClient ) {
+  constructor(private http: HttpClient) {
     console.log('Poke Service Ready');
   }
 
-  getQuery(search: string){
-    const URL = `https:/pokeapi.co/api/v2/${search}`;
+  getQuery(search: string): Observable<any> {
+    const URL = `https://pokeapi.co/api/v2/${search}`;
     return this.http.get(URL);
   }
 
-  getAllPokemons(){
+  getAllPokemons(): Observable<any[]> {
     return this.getQuery('pokemon?limit=150')
       .pipe(
-        mergeMap( results => results['results'] ),          // Get name and urls
-        map( urls => urls['url'] ),                         // Get only urls
-        mergeMap( pokemon => this.getInfoPokemon(pokemon) ),// Get all info of pokemon (withouth description)
-        map( result =>  this.returnRelevant(result) ),      // Get relevant information
-        toArray()
-     ) ;
+        concatMap(results => results['results']),            // Get name and urls
+        concatMap(urls => this.getInfoPokemon(urls['url'])), // Get all info of pokemon (without description)
+        concatMap(result => this.returnRelevant(result)),    // Get relevant information
+        toArray()                                            // Collect all results into an array
+      );
   }
 
-  returnRelevant(result)
-  {
-    const pokemon = {
-      name:  result['name'],
-      stats: result['stats'],
-      types: result['types'],
-      image: result['sprites'].front_default,
-      description: this.getInfoPokemon(result['species'].url)
-        .pipe(
-          map(res => res['flavor_text_entries']),
-          map( res => res[42] ),
-          map(res => res['flavor_text']))
-        .subscribe( res => pokemon.description = res )
-    }
-    return pokemon;
+  returnRelevant(result: any): Observable<any> {
+    return this.getInfoPokemon(result['species'].url)
+      .pipe(
+        map(speciesInfo => {
+          const descriptionEntry = speciesInfo['flavor_text_entries'].find((entry: any) => entry.language.name === 'en');
+          const pokemon = {
+            name: result['name'],
+            stats: result['stats'],
+            types: result['types'],
+            image: result['sprites'].front_default,
+            description: descriptionEntry ? descriptionEntry['flavor_text'] : 'No description available'
+          };
+          return pokemon;
+        })
+      );
   }
 
-  getInfoPokemon(id: string){
+  getInfoPokemon(id: string): Observable<any> {
     return this.http.get(id);
   }
 
